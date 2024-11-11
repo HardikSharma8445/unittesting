700cb732cd79b3cfa887563ba36eedcca08b35c2
HardikSharma8445 <hardik.sharma8532@gmail.com>
2024-11-11 11:46:31 +0530

    hello

--- hello.java
+++ hello.java
@@ -1,7 +1,19 @@
-// Your First Program
-
-class HelloWorld {
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+  
+      String str = "Radar", reverseStr = "";
+      
+      int strLength = str.length();
+  
+      for (int i = (strLength - 1); i >=0; --i) {
+        reverseStr = reverseStr + str.charAt(i);
+      }
+  
+      if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+        System.out.println(str + " is a Palindrome String.");
+      }
+      else {
+        System.out.println(str + " is not a Palindrome String.");
+      }
     }
-}
+  }
